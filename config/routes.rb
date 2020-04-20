d53b2e6c734ef6994c16fef125ae6886a2892536
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-20 19:22:44 +0530

    Adding HTML and CSS files for HomePage, Sign-In and Sign-Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Resolving dependencies...
-Rails.application.routes.draw do
-  get "/" => "home#index"
+Resolving dependencies...Rails.application.routes.draw do
+  #get "/" => "home#index"
+
+  #get "/signin" => "sessions#new", as: :new_sessions
+  #post "/signin" => "sessions#create", as: :sessions
+  #delete "/signout" => "sessions#destroy", as: :destroy_session
 end
