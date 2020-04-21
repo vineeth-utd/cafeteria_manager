c89615d7aab90a92fea5501cbb280bc73cfca005
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-21 11:53:46 +0530

    Added css to homepage,signin,signup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :email, presence: true
+
+  has_secure_password
+
   has_many :orders
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "home#index"
 
-  #get "/signin" => "sessions#new", as: :new_sessions
-  #post "/signin" => "sessions#create", as: :sessions
-  #delete "/signout" => "sessions#destroy", as: :destroy_session
+  resources :users
+
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
