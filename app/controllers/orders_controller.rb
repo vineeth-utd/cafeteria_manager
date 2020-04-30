527040f9b6b17f51cb1367b37a00b23087947c94
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-04-30 18:48:19 +0530

    Now user can see only his cart items and orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,17 @@
 class OrdersController < ApplicationController
   def index
+    @orders = Order.where(user_id: current_user.id)
     render "index"
   end
 
   def new
+    @orders = Order.where(user_id: current_user.id)
     render "new"
   end
 
   def update
     order = Order.find_by(id: params[:order_id])
+    #order = Order.where(user_id: current_user.id).find_by(id: params[:order_id])
     #order.status = "order_confirmed"
     order.update!(status: "order_confirmed")
     #order.save!
