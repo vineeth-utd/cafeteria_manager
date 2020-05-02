05227feee3ccda6aae569387ad6d8c3f93e40d46
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-05-02 14:10:24 +0530

    Now manager can edit or delete menuitem

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -4,7 +4,10 @@ class MenuItemsController < ApplicationController
     render "index"
   end
 
-  def new
+  def edit
+    @menuitem = Menuitem.find(params[:id])
+    @categories = Menuitem.distinct.pluck(:category)
+    render "edit"
   end
 
   def create
@@ -16,6 +19,25 @@ class MenuItemsController < ApplicationController
       price: params[:price],
       category: params[:category],
     )
+    redirect_to menu_items_path
+  end
+
+  def update
+    menu = Menu.active
+    menuitem = Menuitem.find(params[:id])
+    menuitem.update(
+      menu_id: menu.id,
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      category: params[:category],
+    )
+    redirect_to menu_items_path
+  end
+
+  def destroy
+    menuitem = Menuitem.find(params[:id])
+    menuitem.destroy
     redirect_to menu_items_path
   end
 end
