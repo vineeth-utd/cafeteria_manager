f0adb90f7b899080c216dc594f9f731341f37446
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-05-16 12:13:01 +0530

    Bug removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to menu_items_path
     else
-      flash[:error] = "Your login attempt was invalid. Please retry."
+      flash[:error] = "Invalid email or password!"
       redirect_to new_sessions_path
     end
   end
