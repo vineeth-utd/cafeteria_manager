5717f6157d00a9f5045cb68b0646ed46d3133576
vineeth-utd <vineeth.jaisankar@gmail.com>
2020-05-26 08:27:54 +0530

    Bug removed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
-
   resources :about
   resources :menus
   resources :menu_items
@@ -14,5 +13,5 @@ Rails.application.routes.draw do
   resources :order_items
   get "/pending_orders", to: "orders#pending_orders", as: :pending_orders
   get "/reports", to: "orders#reports", as: :reports
-  get "/invoice", to: "orders#invoice", as: :invoice
+  post "/invoice/:id", to: "orders#invoice", as: :invoice
 end
